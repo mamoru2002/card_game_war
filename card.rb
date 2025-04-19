7a3cb290e8d78182bf3a2883d18cc50d914ba4d2
mamoru2002 <tiusika.m@gmail.com>
2025-04-19 23:12:59 +0900

    feat: 山札取得のためCardクラスにdeckメソッドを追加

--- card.rb
+++ card.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Card
+  SUITS = %w[ハート ダイヤ クラブ スペード].freeze
+  RANKS = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
+  attr_reader :deck
+
+  def initialize
+    @deck = []
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        deck << [suit, rank]
+      end
+    end
+  end
 end
