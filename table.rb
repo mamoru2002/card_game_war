d05f964ead9b9bf17abae24174c77b640c68b6f9
mamoru2002 <tiusika.m@gmail.com>
2025-04-23 11:59:53 +0900

    refactor: battleメソッドを分割し、役割ごとに補助メソッドを定義

--- table.rb
+++ table.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'pp'
 require_relative 'card'
 require_relative 'game'
 require_relative 'player'
@@ -17,47 +16,51 @@ class Table
     @ranks = []
   end
 
+  def draw_battle_layout
+    battle_layout = []
+    @players.each do |player|
+      battle_layout << player.deal.shift
+    end
+    battle_layout
+  end
+
+  def draw_suits_ranks(battle_layout)
+    suits = []
+    ranks = []
+    battle_layout.map do |suit, rank|
+      suits << suit
+      ranks << rank
+    end
+    [suits, ranks]
+  end
+
+  def puts_battle_layout(suits, ranks)
+    @game.player_count.times do |i|
+      puts "#{@players[i].name}のカードは#{suits[i]}の#{ranks[i]}です。"
+    end
+  end
+
+  def battle_result(winner_index)
+    puts "#{@players[winner_index].name}が勝ちました。#{@players[winner_index].name}はカードを#{@layout.size}枚もらいました。"
+    @players[winner_index].add_deal(*@layout)
+    @layout = []
+  end
+
   def battle
     loop do
       break if @players.any? { |player| player.deal.empty? }
 
-      @suits = []
-      @ranks = []
-      battle_layout = []
-
-      @players.each do |player|
-        battle_layout << player.deal.shift
-      end
-
+      battle_layout = draw_battle_layout
+      suits, ranks = draw_suits_ranks(battle_layout)
       @layout.concat(battle_layout)
-
-      battle_layout.map do |suit, rank|
-        @suits << suit
-        @ranks << rank
-      end
-
       puts '戦争！'
-
-      @game.player_count.times do |i|
-        puts "#{@players[i].name}のカードは#{@suits[i]}の#{@ranks[i]}です。"
-      end
-
-      rank_layout = battle_layout.map { |rank| rank[1] }
-
-      rank_layout = rank_layout.map { |rank| Card::RANKS.index(rank) }
-
-      min_rank = rank_layout.min
-
-      if rank_layout.count(min_rank) > 1
+      puts_battle_layout(suits, ranks)
+      ranks_strength = ranks.map { |rank| Card::RANKS.index(rank) }
+      if ranks_strength.count(ranks_strength.min) > 1
         puts '引き分けです。'
         next
       end
-
-      winner_index = rank_layout.index(rank_layout.min)
-
-      puts "#{@players[winner_index].name}が勝ちました。#{@players[winner_index].name}はカードを#{@layout.size}枚もらいました。"
-      @players[winner_index].add_deal(*@layout)
-      @layout = []
+      battle_result(ranks_strength.index(ranks_strength.min))
     end
   end
 end
