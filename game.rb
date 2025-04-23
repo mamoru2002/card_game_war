932fdc3f7676fc45cf2743b831db27475acfe64a
mamoru2002 <tiusika.m@gmail.com>
2025-04-23 18:43:31 +0900

    refactor: GameとTableクラスの処理を整理し、Rubocopの全警告（6件）を解消

--- game.rb
+++ game.rb
@@ -10,33 +10,47 @@ class Game
     @table = Table.new(self, @players)
 
     puts '戦争を開始します。'
-    print 'プレイヤーの人数を入力してください（2〜5）:'
-    @player_count = gets.chomp.to_i
+    setup_players
 
-    @player_count.times do |i|
-      print "プレイヤー#{i + 1}の名前を入力してください:"
-      name = gets.chomp
-      @players << Player.new(self, name)
-    end
-
-    @players.each { |player| player.deal_cards(@player_count) }
-    puts 'カードが配られました。'
+    deal_cards_players
 
     @table.battle
-    lose_index = (@players.map { |player| player.deal.size }).index(&:zero?)
-
-    puts "#{@players[lose_index].name}の手札がなくなりました。"
-    @players.each do |player|
-      print "#{player.name}の手札は#{player.deal.size}枚です。"
-    end
-
-    @players = @players.sort_by { |player| player.deal.size }.reverse
-    @players.each.with_index do |player, i|
-      print "#{player.name}が#{i + 1}位"
-      print '、' if i != @player_count - 1
-    end
-    puts 'です。'
+    show_result
 
     puts '戦争を終了します。'
   end
+end
+
+def setup_players
+  print 'プレイヤーの人数を入力してください（2〜5）:'
+  @player_count = gets.chomp.to_i
+
+  @player_count.times do |i|
+    print "プレイヤー#{i + 1}の名前を入力してください:"
+    name = gets.chomp
+    @players << Player.new(self, name)
+  end
+end
+
+def deal_cards_players
+  @players.each { |player| player.deal_cards(@player_count) }
+  puts 'カードが配られました。'
+end
+
+def show_result
+  lose_index = (@players.map { |player| player.deal.size }).index(&:zero?)
+  puts "#{@players[lose_index].name}の手札がなくなりました。"
+  @players.each do |player|
+    print "#{player.name}の手札は#{player.deal.size}枚です。"
+  end
+  result_ranking
+end
+
+def result_ranking
+  @players = @players.sort_by { |player| player.deal.size }.reverse
+  @players.each.with_index do |player, i|
+    print "#{player.name}が#{i + 1}位"
+    print '、' if i != @player_count - 1
+  end
+  puts 'です。'
 end
--- table.rb
+++ table.rb
@@ -16,7 +16,36 @@ class Table
     @ranks = []
   end
 
-  def draw_battle_layout
+  def battle
+    loop do
+      break if battle_finish?
+
+      play_one_round
+    end
+  end
+
+  def battle_finish?
+    return false unless @players.any? { |player| player.deal.empty? }
+
+    true
+  end
+
+  def play_one_round
+    puts '戦争！'
+    battle_layout = collect_battle_layout
+
+    @layout.concat(battle_layout)
+    return if handle_joker(battle_layout)
+
+    suits, ranks = draw_suits_ranks(battle_layout)
+    puts_battle_layout(suits, ranks)
+    ranks_strength = ranks.map { |rank| Card::RANKS.index(rank) }
+    return if handle_a_spade(ranks, suits) || handle_draw(ranks_strength)
+
+    battle_result(ranks_strength.index(ranks_strength.min))
+  end
+
+  def collect_battle_layout
     battle_layout = []
     @players.each do |player|
       battle_layout << player.deal.shift
@@ -41,36 +70,31 @@ class Table
   end
 
   def battle_result(winner_index)
-    puts "#{@players[winner_index].name}が勝ちました。#{@players[winner_index].name}はカードを#{@layout.size}枚もらいました。"
+    puts "#{@players[winner_index].name}が勝ちました。"
+    print "#{@players[winner_index].name}はカードを#{@layout.size}枚もらいました。"
     @players[winner_index].add_deal(*@layout)
     @layout = []
   end
 
-  def battle
-    loop do
-      break if @players.any? { |player| player.deal.empty? }
+  def handle_joker(battle_layout)
+    return false unless battle_layout.include?('ジョーカー')
 
-      puts '戦争！'
-      battle_layout = draw_battle_layout
+    print "#{@players[battle_layout.index('ジョーカー')].name}がジョーカーを出しました。"
+    battle_result(battle_layout.index('ジョーカー'))
+    true
+  end
 
-      @layout.concat(battle_layout)
-      if battle_layout.include?('ジョーカー')
-        print "#{@players[battle_layout.index('ジョーカー')].name}がジョーカーを出しました。"
-        battle_result(battle_layout.index('ジョーカー'))
-        next
-      end
-      suits, ranks = draw_suits_ranks(battle_layout)
-      puts_battle_layout(suits, ranks)
-      ranks_strength = ranks.map { |rank| Card::RANKS.index(rank) }
-      if ranks.count('A') > 1 && suits[ranks.index('A')] == 'スペード'
-        battle_result(ranks.index('A'))
-        next
-      end
-      if ranks_strength.count(ranks_strength.min) > 1
-        puts '引き分けです。'
-        next
-      end
-      battle_result(ranks_strength.index(ranks_strength.min))
-    end
+  def handle_a_spade(ranks, suits)
+    return false unless ranks.count('A') > 1 && suits[ranks.index('A')] == 'スペード'
+
+    battle_result(ranks.index('A'))
+    true
+  end
+
+  def handle_draw(ranks_strength)
+    return false unless ranks_strength.count(ranks_strength.min) > 1
+
+    puts '引き分けです。'
+    true
   end
 end
